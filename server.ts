2f342c5634e51d1fda0fd842123604cd4d3bb0b5
chile4coding <chileomereji@gmail.com>
2023-09-10 16:33:28 +0100

    cleaned up code

--- server.ts
+++ server.ts
@@ -12,13 +12,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors({ credentials: true, origin: "*" }));
 
 app.use(route);
-
+const PORT = process.env.PORT || 3000;
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   const statusCode = error.statusCode || 500;
   const message = error.message || "INVALID REQUEST";
 
   res.status(statusCode).json({ error: message });
 });
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`listening on ${process.env.PORT}`);
 });
